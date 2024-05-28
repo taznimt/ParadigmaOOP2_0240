dfeb420f388697e397169ec410c1186985c14d85
taznimt <taznimanwar06@gmail.com>
2024-05-28 14:53:41 +0700

    create member inisialisasi dari class orang

--- solvemutiwaris/solvemutiwaris.cpp
+++ solvemutiwaris/solvemutiwaris.cpp
@@ -4,6 +4,12 @@ using namespace std;
 class orang {
 public:
     int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
 };
 int main()
 {
